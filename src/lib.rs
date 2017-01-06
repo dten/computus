c90686f5ff03be11e07f3e7c2fbb7414c1d55e88
David Hewson <dev@daveid.co.uk>
2017-01-06 23:34:40 +0000

    result instead of panic
    also better range

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
-fn check_easter_happened(year: i32) {
-    if year < 33 {
-        panic!(format!("Jesus isn't dead yet in the year {}", year));
-    }
-}
+const OUT_OF_RANGE_ERR: &'static str = "Computus is only valid from 1583 to 9999";
 
 pub mod gregorian {
     /// Easter in the Gregorian calendar
-    pub fn month_day(year: i32) -> (u32, u32) {
-        super::check_easter_happened(year);
+    pub fn month_day(year: i32) -> Result<(u32, u32), &'static str> {
+        if year < 1583 || year > 9999 {
+            return Err(::OUT_OF_RANGE_ERR);
+        }
         let aa = year % 19;
         let bb = year / 100;
         let cc = year % 100;
@@ -22,14 +20,16 @@ pub mod gregorian {
         let mm = (aa + 11 * hh + 22 * ll) / 451;
         let month = (hh + ll - 7 * mm + 114) / 31;
         let day = (hh + ll - 7 * mm + 114) % 31 + 1;
-        (month as u32, day as u32)
+        Ok((month as u32, day as u32))
     }
 }
 
 pub mod julian {
     /// Easter in the Julian calendar
-    pub fn month_day(year: i32) -> (u32, u32) {
-        super::check_easter_happened(year);
+    pub fn month_day(year: i32) -> Result<(u32, u32), &'static str> {
+        if year < 1583 || year > 9999 {
+            return Err(::OUT_OF_RANGE_ERR);
+        }
         let aa = year % 4;
         let bb = year % 7;
         let cc = year % 19;
@@ -38,7 +38,7 @@ pub mod julian {
         let ff = dd + ee + 114;
         let month = ff / 31;
         let day = ff % 31 + 1;
-        (month as u32, day as u32)
+        Ok((month as u32, day as u32))
     }
 }
 
@@ -46,62 +46,67 @@ pub mod julian {
 mod tests {
     #[test]
     fn gregorian_month_day() {
-        use super::gregorian::month_day;
-        assert_eq!(month_day(1961), (4, 2));
-        assert_eq!(month_day(1996), (4, 7));
-        assert_eq!(month_day(1997), (3, 30));
-        assert_eq!(month_day(1998), (4, 12));
-        assert_eq!(month_day(2000), (4, 23));
-        assert_eq!(month_day(2001), (4, 15));
-        assert_eq!(month_day(2002), (3, 31));
-        assert_eq!(month_day(2003), (4, 20));
-        assert_eq!(month_day(2004), (4, 11));
-        assert_eq!(month_day(2005), (3, 27));
-        assert_eq!(month_day(2006), (4, 16));
-        assert_eq!(month_day(2007), (4, 8));
-        assert_eq!(month_day(2008), (3, 23));
-        assert_eq!(month_day(2009), (4, 12));
-        assert_eq!(month_day(2010), (4, 4));
-        assert_eq!(month_day(2011), (4, 24));
-        assert_eq!(month_day(2012), (4, 8));
-        assert_eq!(month_day(2013), (3, 31));
-        assert_eq!(month_day(2014), (4, 20));
-        assert_eq!(month_day(2015), (4, 5));
-        assert_eq!(month_day(2016), (3, 27));
-        assert_eq!(month_day(2017), (4, 16));
-        assert_eq!(month_day(2018), (4, 1));
-        assert_eq!(month_day(2019), (4, 21));
-        assert_eq!(month_day(2020), (4, 12));
+        use super::gregorian::month_day as gregorian;
+        assert_eq!(gregorian(1961), Ok((4, 2)));
+        assert_eq!(gregorian(1996), Ok((4, 7)));
+        assert_eq!(gregorian(1997), Ok((3, 30)));
+        assert_eq!(gregorian(1998), Ok((4, 12)));
+        assert_eq!(gregorian(2000), Ok((4, 23)));
+        assert_eq!(gregorian(2001), Ok((4, 15)));
+        assert_eq!(gregorian(2002), Ok((3, 31)));
+        assert_eq!(gregorian(2003), Ok((4, 20)));
+        assert_eq!(gregorian(2004), Ok((4, 11)));
+        assert_eq!(gregorian(2005), Ok((3, 27)));
+        assert_eq!(gregorian(2006), Ok((4, 16)));
+        assert_eq!(gregorian(2007), Ok((4, 8)));
+        assert_eq!(gregorian(2008), Ok((3, 23)));
+        assert_eq!(gregorian(2009), Ok((4, 12)));
+        assert_eq!(gregorian(2010), Ok((4, 4)));
+        assert_eq!(gregorian(2011), Ok((4, 24)));
+        assert_eq!(gregorian(2012), Ok((4, 8)));
+        assert_eq!(gregorian(2013), Ok((3, 31)));
+        assert_eq!(gregorian(2014), Ok((4, 20)));
+        assert_eq!(gregorian(2015), Ok((4, 5)));
+        assert_eq!(gregorian(2016), Ok((3, 27)));
+        assert_eq!(gregorian(2017), Ok((4, 16)));
+        assert_eq!(gregorian(2018), Ok((4, 1)));
+        assert_eq!(gregorian(2019), Ok((4, 21)));
+        assert_eq!(gregorian(2020), Ok((4, 12)));
+        assert_eq!(gregorian(2021), Ok((4, 4)));
+        assert_eq!(gregorian(2022), Ok((4, 17)));
+        assert_eq!(gregorian(3035), Ok((4, 19)));
+        assert_eq!(gregorian(4000), Ok((4, 9)));
+        assert_eq!(gregorian(9999), Ok((3, 28)));
     }
 
     #[test]
     fn julian_month_day() {
-        use super::julian::month_day;
-        assert_eq!(month_day(1961), (3, 27));
-        assert_eq!(month_day(1996), (4, 1));
-        assert_eq!(month_day(1997), (4, 14));
-        assert_eq!(month_day(1998), (4, 6));
-        assert_eq!(month_day(1999), (3, 29));
-        assert_eq!(month_day(2000), (4, 17));
-        assert_eq!(month_day(2001), (4, 2));
-        assert_eq!(month_day(2002), (4, 22));
-        assert_eq!(month_day(2003), (4, 14));
-        assert_eq!(month_day(2004), (3, 29));
-        assert_eq!(month_day(2005), (4, 18));
-        assert_eq!(month_day(2006), (4, 10));
-        assert_eq!(month_day(2007), (3, 26));
-        assert_eq!(month_day(2008), (4, 14));
-        assert_eq!(month_day(2009), (4, 6));
-        assert_eq!(month_day(2010), (3, 22));
-        assert_eq!(month_day(2011), (4, 11));
-        assert_eq!(month_day(2012), (4, 2));
-        assert_eq!(month_day(2013), (4, 22));
-        assert_eq!(month_day(2014), (4, 7));
-        assert_eq!(month_day(2015), (3, 30));
-        assert_eq!(month_day(2016), (4, 18));
-        assert_eq!(month_day(2017), (4, 3));
-        assert_eq!(month_day(2018), (3, 26));
-        assert_eq!(month_day(2019), (4, 15));
-        assert_eq!(month_day(2020), (4, 6));
+        use super::julian::month_day as julian;
+        assert_eq!(julian(1961), Ok((3, 27)));
+        assert_eq!(julian(1996), Ok((4, 1)));
+        assert_eq!(julian(1997), Ok((4, 14)));
+        assert_eq!(julian(1998), Ok((4, 6)));
+        assert_eq!(julian(1999), Ok((3, 29)));
+        assert_eq!(julian(2000), Ok((4, 17)));
+        assert_eq!(julian(2001), Ok((4, 2)));
+        assert_eq!(julian(2002), Ok((4, 22)));
+        assert_eq!(julian(2003), Ok((4, 14)));
+        assert_eq!(julian(2004), Ok((3, 29)));
+        assert_eq!(julian(2005), Ok((4, 18)));
+        assert_eq!(julian(2006), Ok((4, 10)));
+        assert_eq!(julian(2007), Ok((3, 26)));
+        assert_eq!(julian(2008), Ok((4, 14)));
+        assert_eq!(julian(2009), Ok((4, 6)));
+        assert_eq!(julian(2010), Ok((3, 22)));
+        assert_eq!(julian(2011), Ok((4, 11)));
+        assert_eq!(julian(2012), Ok((4, 2)));
+        assert_eq!(julian(2013), Ok((4, 22)));
+        assert_eq!(julian(2014), Ok((4, 7)));
+        assert_eq!(julian(2015), Ok((3, 30)));
+        assert_eq!(julian(2016), Ok((4, 18)));
+        assert_eq!(julian(2017), Ok((4, 3)));
+        assert_eq!(julian(2018), Ok((3, 26)));
+        assert_eq!(julian(2019), Ok((4, 15)));
+        assert_eq!(julian(2020), Ok((4, 6)));
     }
 }
